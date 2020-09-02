41e15c5d1a76ec7e003048cc2798509834550752
Michal Stano <mchl.stano@gmail.com>
2020-09-02 20:03:07 +0200

    Added missing changeDetector

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
       home-page works!
     </p>
   `,
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePageComponent implements OnInit {
   constructor() {}
