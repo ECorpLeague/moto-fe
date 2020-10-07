78e12bcc32d6871f32e626c94d710f724d1ce27f
Michal Stano <mchl.stano@gmail.com>
2020-10-07 20:36:53 +0200

    Refactor responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 import { SettingsActions } from './settings/+state/settings.actions';
 import { SettingsFacade } from './settings/+state/settings.facade';
 @Component({
   selector: 'app-root',
   template: `
     <app-header></app-header>
-    <div class="main">
+    <div class="main" #main>
       <div class="content">
         <router-outlet></router-outlet>
       </div>
@@ -18,11 +28,33 @@ import { SettingsFacade } from './settings/+state/settings.facade';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('main') main: ElementRef;
+  private destroy$ = new Subject<void>();
+
   constructor(
     public settingsFacade: SettingsFacade,
-    private store: Store<any>
+    private store: Store<any>,
+    private router: Router
   ) {
     this.store.dispatch(SettingsActions.init());
+  }
+
+  ngOnInit(): void {
+    this.backToTopIfRouterChanged();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private backToTopIfRouterChanged(): void {
+    this.router.events
+      .pipe(
+        filter(x => x instanceof NavigationEnd),
+        takeUntil(this.destroy$)
+      )
+      .subscribe(() => this.main.nativeElement.scrollTo(0, 0));
   }
 }
